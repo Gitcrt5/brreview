a63014ece32ae64ea31408fbc89cf89e4119a769
CRT <153168097+Gitcrt5@users.noreply.github.com>
2025-04-20 20:08:52 +1000

    Update update.sh
    Run update sxcript using sudo

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Updating brreview..."
 git pull || exit 1
-docker-compose build || exit 1
-docker-compose up -d || exit 1
+sudo docker-compose build || exit 1
+sudo docker-compose up -d || exit 1
 echo "Update complete."
